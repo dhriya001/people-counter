ced179b66d40e61f4223d517e0dbf68fffe03dab
dhriya001 <132187164+dhriya001@users.noreply.github.com>
2023-04-30 12:34:00 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,9 +1,11 @@
 let countIt = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 function increment() {
 	count = count + 1
-	countIt.innerText = count
+	countIt.textContent = count
 }
+ 
 function save() {
         let countStr = count + " + "
         saveEl.textContent += countStr
@@ -11,3 +13,4 @@ function save() {
         count = 0
 
 }
+console.log("lets count people")
