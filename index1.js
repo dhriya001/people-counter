0149f0481595f17e373ee02008f8e4b8d8979188
dhriya001 <132187164+dhriya001@users.noreply.github.com>
2023-04-30 11:58:32 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -4,3 +4,10 @@ function increment() {
 	count = count + 1
 	countIt.innerText = count
 }
+function save() {
+        let countStr = count + " + "
+        saveEl.textContent += countStr
+        countIt.textContent = 0;
+        count = 0
+
+}
